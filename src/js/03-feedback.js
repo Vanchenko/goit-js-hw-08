29b7c4dcd821f489608ab99b87071eb4e2bde6d5
Vanchenko <vanchenkoev@pakline.com.ua>
2023-03-30 23:39:37 +0300

    обновил задачу 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,26 @@
 import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector(".feedback-form");
-const usdate = JSON.parse(localStorage.getItem("feedback-form-state"));
+const usdate = {
+    email: "",
+    message: "",
+};
+const pp = JSON.parse(localStorage.getItem("feedback-form-state"));
+console.log(pp)
+
+if (pp) {
+    console.log("true")
+    usdate.email = pp.email;
+    usdate.message = pp.message;
+    };
+
+console.log(usdate);
+
+formEl.elements[0].value = usdate.email;
+formEl.elements[1].value = usdate.message;
 
 formEl.addEventListener("submit", throttle(checkInput, 500));
 formEl.addEventListener("input", throttle(checkInput, 500));
-
-if (usdate.email) { formEl.elements[0].value = usdate.email }
-else { formEl.elements[0].value = "" };
-if (usdate.message) { formEl.elements[1].value = usdate.message }
-else { formEl.elements[1].value = "" };
 
 function checkInput(event) {
     event.preventDefault();
@@ -20,11 +31,10 @@ function checkInput(event) {
         case "TEXTAREA": usdate.message = event.target.value;
             break;
         case "FORM": console.log(usdate);
-            usdate.email = "";
-            usdate.message = "";
-            formEl.elements[0].value = usdate.email;
-            formEl.elements[1].value = usdate.message;
-            break;
+            localStorage.removeItem("feedback-form-state");
+            formEl.elements[0].value = "";
+            formEl.elements[1].value = "";
+            return;
     };
     localStorage.setItem("feedback-form-state", JSON.stringify(usdate));
 };
